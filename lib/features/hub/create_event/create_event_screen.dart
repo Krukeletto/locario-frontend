e8b5d461a792ab643c9ed1ebf751809fb814d57b
Krukeletto <bmichal2003@gmail.com>
2026-06-21 22:03:04 +0200

    fix: suppress deprecation warning for onReorder in selected images list

--- lib/features/hub/create_event/create_event_screen.dart
+++ lib/features/hub/create_event/create_event_screen.dart
@@ -681,7 +681,8 @@ class _SelectedImagesList extends StatelessWidget {
         buildDefaultDragHandles: true,
         physics: const NeverScrollableScrollPhysics(),
         itemCount: images.length,
-        onReorderItem: onReorder,
+        // ignore: deprecated_member_use
+        onReorder: onReorder,
         itemBuilder: (context, index) {
           final image = images[index];
           final isPrimary = index == 0;
